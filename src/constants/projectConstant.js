814685278638f03d659e63a2373f6f9ead4d9368
morabagipravin <morabagipravin687@gmail.com>
2025-01-18 01:35:23 +0530

    Added a new project

--- src/constants/projectConstant.js
+++ src/constants/projectConstant.js
@@ -1,7 +1,7 @@
-import PortfolioImg from "../assets/portfolio.jpeg";
-import SuicidePrevention from "../assets/SuicidePrevention.jpeg";
-import ToDoMaster from "../assets/ToDoMaster.jpeg";
-import TextNova from "../assets/TextNova.jpeg";
+import SuicidePreventionImage from "../assets/SuicidePrevention.jpeg";
+import ToDoMasterImage from "../assets/ToDoMaster.jpeg";
+import TextNovaImage from "../assets/TextNova.jpeg";
+import RiskyRollImage from "../assets/RiskyRoll.jpeg";
 
 const projectsData = [
   {
@@ -16,7 +16,7 @@ const projectsData = [
       "NLTK",
       "ML",
     ],
-    image: SuicidePrevention,
+    image: SuicidePreventionImage,
     github: "https://github.com/morabagipravin/Suicide-Risk-Detection",
     website: "",
   },
@@ -25,7 +25,7 @@ const projectsData = [
     name: "To do Master",
     description: "Developed a React-based To-Do manager, containerized with Docker for efficient task management.",
     techStack: ["JavaScript", "React", "CSS", "GSAP", "Docker"],
-    image: ToDoMaster,
+    image: ToDoMasterImage,
     github: "https://github.com/morabagipravin/To-Do-Master",
     website: "",
   },
@@ -34,10 +34,20 @@ const projectsData = [
     name: "Text Nova",
     description: "A website offering various text-based tools and utilities.",
     techStack: ["HTML", "CSS", "JavaScript", "React", "Bootstrap5"],
-    image: TextNova,
+    image: TextNovaImage,
     github: "https://github.com/morabagipravin/TextNova",
     website: "",
   },
+  {
+    id: 4,
+    name: "Risky-Roll",
+    description: "Created a 2-player JavaScript game where the goal is to reach 100 points by rolling dice. Be cautious, rolling a 1 resets your current points.",
+    techStack: ["HTML", "CSS", "JavaScript"],
+    image: RiskyRollImage,
+    github: "https://github.com/morabagipravin/Risky-Roll",
+    website: "",
+},
+
 ];
 
 export default projectsData;
